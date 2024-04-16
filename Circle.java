79c45b3e6efef3f511ec74a2cc6f67e660bb59d9
thanhhang31023 <153881344+thanhhang31023@users.noreply.github.com>
2024-04-16 14:55:22 +0900

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -14,7 +14,7 @@ public class Circle {
         double radius = scanner.nextDouble();
 
         double circumference = 2 * Math.PI * radius;
-        double area = Math.PI * Math.pow(radius, 2);
+        double area = Math.PI * (radius* radius);
 
         System.out.println("Chu vi của hình tròn là: " + circumference);
         System.out.println("Diện tích của hình tròn là: " + area);
