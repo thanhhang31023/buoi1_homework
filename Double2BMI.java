ce34651f1f88e3ab13dadb782d3610482320d753
thanhhang31023 <153881344+thanhhang31023@users.noreply.github.com>
2024-04-16 14:51:43 +0900

    Update Double2BMI.java

--- Double2BMI.java
+++ Double2BMI.java
@@ -29,7 +29,7 @@ public class Double2BMI {
         }
 
         public static double calculateBMI(double weight, double height) {
-            return weight / Math.pow(height, 2);
+            return weight / (height*height);
         }
     }
 
